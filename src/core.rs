Ansvia/reframe

commit ab886b5fd78da093e7ab836de4bace70ec7385dd

bug fix: pre_params overwritten by new_params

src/core.rs
@@ -7,7 +7,7 @@ use lazy_static::lazy_static;
 use log::{debug, error, trace};
 use regex::Regex;
 use rustyline::Editor;
-use serde::{Deserialize};
+use serde::Deserialize;
 use serde_json::Value as JsonValue;
 
 use crate::util;
@@ -268,7 +268,8 @@ impl<'a> Reframe<'a> {
         params
             .iter()
             .find(|a| a.key == k)
-            .map(|a| a.value.as_ref().unwrap().to_owned())
+            .and_then(|a| a.value.as_ref())
+            .map(|a| a.to_owned())
     }
 
     fn param_value(params: &[Param], k: &str) -> String {
@@ -381,6 +382,8 @@ impl<'a> Reframe<'a> {
                 _ => Either::Right(p.clone()),
             });
 
+        let mut final_params = pre_params.clone();
+
         for p in new_params.iter_mut() {
             if let Some(depends) = p.ifwith.as_ref() {
                 if Self::param_value(&self.params, depends) == "false" {
@@ -435,6 +438,7 @@ impl<'a> Reframe<'a> {
                 self.rl.add_history_entry(rv.clone());
 
                 p.value = Some(rv);
+                final_params.push(p.clone());
 
                 break;
             }
@@ -456,11 +460,13 @@ impl<'a> Reframe<'a> {
                 );
             }
 
-            self.params
+            final_params
                 .iter_mut()
                 .find(|a| a.key == p.key)
                 .map(|a| a.value = p.value.to_owned());
         }
+
+        self.params = final_params;
 
         let out_dir = out_name
             .as_ref()
@@ -643,7 +649,7 @@ impl<'a> Reframe<'a> {
     pub fn process_template_str(
         text: String,
         config: &Config,
-        param: &[Param],
+        params: &[Param],
         builtin_vars: &[BuiltinVar],
     ) -> String {
         let lines: Vec<&str> = text.split('\n').collect();
@@ -674,7 +680,7 @@ impl<'a> Reframe<'a> {
                 last_if_cond = line;
                 last_if_cond_line = i;
                 let mut if_handled = false;
-                for p in param.iter() {
+                for p in params.iter() {
                     let k = &p.key;
                     let v = match p.value.as_ref() {
                         Some(v) => v,
@@ -734,7 +740,7 @@ impl<'a> Reframe<'a> {
             new_lines2.push(Self::string_sub(
                 line.to_owned(),
                 config,
-                param,
+                params,
                 builtin_vars,
             ));
         }

src/test_conditional_handlebars.rs
@@ -1,8 +1,4 @@
-
-
 use crate::core::*;
-
-
 
 fn build_config(name: &str) -> Config {
     Config {

Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "reframe"
-version = "0.5.0"
+version = "0.5.1"
 authors = ["robin <r@ansvia.com>"]
 edition = "2021"
 description = "Light, Fast & Simple project scaffolder"

tests/basic.rs
@@ -1,9 +1,5 @@
-
-
 // #[macro_use]
-use reframe::core::{*};
-
-
+use reframe::core::*;
 
 fn build_config(name: &str) -> Config {
     Config {

commit 2dd0c53b8712cc81861cd9677e1028ac9294edf5

bug fix: params not cased, caused by: ab886b5 bug fix: pre_params overwritten by new_params

src/core.rs
@@ -447,7 +447,7 @@ impl<'a> Reframe<'a> {
             if p.kind == ParamKind::String {
                 make_case_variant!(
                     p,
-                    self.params,
+                    final_params,
                     [
                         ["lower_case", to_lowercase],
                         ["upper_case", to_uppercase],

src/lib.rs
@@ -2,4 +2,4 @@ pub mod core;
 mod util;
 
 #[cfg(test)]
-mod test_conditional_handlebars;
+mod test_handlebars;

src/test_conditional_handlebars.rs
@@ -1,160 +1,0 @@
-use crate::core::*;
-
-fn build_config(name: &str) -> Config {
-    Config {
-        reframe: ReframeConfig {
-            name: "My Reframe".to_string(),
-            author: "robin".to_string(),
-            min_version: "0.1.0".to_string(),
-        },
-        project: ProjectConfig {
-            name: name.to_owned(),
-            variants: Default::default(),
-            version: "0.1.1".to_string(),
-            ignore_dirs: None,
-            ignore_files: None,
-            finish_text: None,
-        },
-        param: vec![],
-        presents: vec![],
-        post_generate: vec![],
-    }
-}
-
-#[test]
-fn test_if_conditional() {
-    let input = r#"
-{{#if with_capped}}
-bool capped = true;
-{{/if}}
-"#;
-
-    let expected1 = r#"
-"#;
-    let expected2 = r#"
-bool capped = true;
-"#;
-
-    let name = "Conditional";
-
-    let config = build_config(name);
-
-    // let p = Param::new("db".to_string(), "sqlite".to_owned());
-
-    let mut param = vec![];
-    // param.push(p);
-
-    let output = Reframe::process_with_handlebars(
-        "ConditionalTest",
-        input.to_string(),
-        &config,
-        &param,
-        &[],
-    )
-    .unwrap();
-    assert_eq!(output, expected1);
-
-    param.push(Param::new("with_capped".to_string(), "true".to_owned()));
-    let output = Reframe::process_with_handlebars(
-        "ConditionalTest",
-        input.to_string(),
-        &config,
-        &param,
-        &[],
-    )
-    .unwrap();
-    assert_eq!(output, expected2);
-
-    param.clear();
-    // param.push(Param::new("with_x".to_string(), "false".to_owned()));
-    // let output = Reframe::process_template_str(input.to_string(), &config, &param, &[]);
-    // assert_eq!(output, expected3);
-}
-
-#[test]
-fn test_if_conditional_nested() {
-    let input = r#"
-{{#if with_capped}}
-bool capped = true;
-int maxTotal = 10;
-{{/if}}
-{{#if with_something}}
-function doSomething(){
-    {{#if with_capped}}
-    require(count <= maxTotal);
-    {{/if}}
-    return true;
-}
-{{/if}}
-"#;
-
-    let expected1 = r#"
-"#;
-    let expected2 = r#"
-bool capped = true;
-int maxTotal = 10;
-"#;
-    let expected3 = r#"
-bool capped = true;
-int maxTotal = 10;
-function doSomething(){
-    require(count <= maxTotal);
-    return true;
-}
-"#;
-    let expected4 = r#"
-function doSomething(){
-    return true;
-}
-"#;
-
-    let name = "Conditional";
-
-    let config = build_config(name);
-
-    let mut param = vec![];
-
-    let output = Reframe::process_with_handlebars(
-        "ConditionalTest",
-        input.to_string(),
-        &config,
-        &param,
-        &[],
-    )
-    .unwrap();
-    assert_eq!(output, expected1);
-
-    param.push(Param::new("with_capped".to_string(), "true".to_owned()));
-    let output = Reframe::process_with_handlebars(
-        "ConditionalTest",
-        input.to_string(),
-        &config,
-        &param,
-        &[],
-    )
-    .unwrap();
-    assert_eq!(output, expected2);
-
-    param.push(Param::new("with_something".to_string(), "true".to_owned()));
-    let output = Reframe::process_with_handlebars(
-        "ConditionalTest",
-        input.to_string(),
-        &config,
-        &param,
-        &[],
-    )
-    .unwrap();
-    assert_eq!(output, expected3);
-
-    param.clear();
-    param.push(Param::new("with_something".to_string(), "true".to_owned()));
-    let output = Reframe::process_with_handlebars(
-        "ConditionalTest",
-        input.to_string(),
-        &config,
-        &param,
-        &[],
-    )
-    .unwrap();
-    assert_eq!(output, expected4);
-}